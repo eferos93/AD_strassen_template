d2871e1bbad4124907509dbb2b76913d57f396e5
Eros <eros.fabrici@gmail.com>
2020-05-29 13:15:31 +0200

    code for non-suqare matrices. Needs testing

--- matrix.h
+++ matrix.h
@@ -3,7 +3,7 @@
 
 void naive_matrix_multiplication(float **C, float const *const *const A,
                                 float const *const *const B,
-                                const size_t n);
+                                const size_t i,const size_t k,const size_t j);
 
 int same_matrix(float const *const *const A, float const *const *const B,
                 const size_t rows, const size_t cols);
@@ -13,4 +13,4 @@ void deallocate_matrix(float **A, const size_t rows);
 
 
 float **allocate_random_matrix(const size_t rows, const size_t cols);
-#endif //__MATRIX__
+#endif
