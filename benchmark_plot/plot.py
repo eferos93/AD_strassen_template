acfbe944da7671e0f62b31572b14bb54fe7b6843
Eros Fabrici <eros.fabrici@gmail.com>
2020-05-30 14:37:09 +0200

    Plots done; readme done; Finished.

--- benchmark_plot/plot.py
+++ benchmark_plot/plot.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-strassen_times = pd.read_csv("benchmark.txt", sep ="\t", header=None, skiprows=1)
+all_times = pd.read_csv("benchmark.txt", sep ="\t", header=None, skiprows=1)
+strassen_times = pd.read_csv("benchmark_strassen.txt", sep="\t", header=None, skiprows=1)
 
-strassen_times.columns = ["n","Naive", "Strassen", "Optimised Strassen", "Same result (true or false)"]
+all_times.columns = ["n","Naive", "Strassen", "Optimised Strassen", "Same result (true or false)"]
+strassen_times.columns = ["n", "Strassen", "Optimised Strassen", "Same result (true or false)"]
 
 fig = plt.figure()
 plt.ylabel("Elapsed Time")
@@ -12,11 +14,25 @@ plt.xlabel("Size")
 
 ax = plt.subplot(111)
 
-ax.plot(data["n"], data["Naive"], label = "Naive")
-ax.plot(data["n"], data["Strassen"], label = "Strassen")
-ax.plot(data["n"], data["Optimised Strassen"], label = "Optimised Strassen")
+ax.plot(all_times["n"], all_times["Naive"], label = "Naive")
+ax.plot(all_times["n"], all_times["Strassen"], label = "Strassen")
+ax.plot(all_times["n"], all_times["Optimised Strassen"], label = "Optimised Strassen")
 
 ax.legend()
 
-plt.savefig("Benchmark.png")
-plt.show("Benchmark.png")
+plt.savefig("benchmark_all.png")
+plt.show("benchmark_all.png")
+
+fig = plt.figure()
+plt.ylabel("Elapsed Time")
+plt.xlabel("Size")
+
+ax = plt.subplot(111)
+
+ax.plot(strassen_times["n"], strassen_times["Strassen"], label = "Strassen")
+ax.plot(strassen_times["n"], strassen_times["Optimised Strassen"], label = "Optimised Strassen")
+
+ax.legend()
+
+plt.savefig("benchmark_strassen.png")
+plt.show("benchmark_strassen.png")
